1d0c81b555820a6ce075e403809c74cf94796249
jeanDeluge <53405354+jeanDeluge@users.noreply.github.com>
2020-09-01 16:48:34 +0900

    [ADD] config, controller, migrations, models/index.ts, .eslintrc.json, app.ts (#39)
    환경설정 중, 기본 sequelize 설치, eslint룰 설치, 서버 열기
    
    Co-authored-by: 홍진영 <jean@MacBook-Pro.local>

---
+++ migrations/20200901063732-create-user.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.STRING
+      },
+      userPassword: {
+        type: Sequelize.STRING
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      birthday: {
+        type: Sequelize.DATE
+      },
+      sex: {
+        type: Sequelize.STRING
+      },
+      interest: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Users');
+  }
+};
